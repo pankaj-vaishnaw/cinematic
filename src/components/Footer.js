b99931a14ca064b9413fe3fc31f1c95a20406ed2
Pankaj Vaishnav <97958495+pankaj-vaishnaw@users.noreply.github.com>
2023-01-12 19:59:20 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import {Box, Typography} from '@mui/material';
+
+const Footer = _ => {
+  return (
+    <Box pb={4}>
+      <Typography variant='h6' align='center'>Developed By  Pankaj Vaishnav</Typography>
+      <Typography variant='h6' align='center'>
+        <a href="https://github.com/aayusharyan/cinematic" target="_blank" rel="noreferrer">
+          Github Source Code
+        </a>
+      </Typography>
+    </Box>
+  )
+}
+
+export default Footer;
